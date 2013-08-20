55081f640a1eafad50bc26c19f3e9154fe6dd0a9
Carlos Rueda <carueda@mbari.org>
2013-08-20 10:30:02 -0700

    test

--- src/test/scala/org/mbari/odss/services/timeline/TimelineServletSpec.scala
+++ src/test/scala/org/mbari/odss/services/timeline/TimelineServletSpec.scala
@@ -3,14 +3,7 @@ package org.mbari.odss.services.timeline
 import org.scalatra.test.specs2._
 
 // For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
-class TimelineServletSpec extends ScalatraSpec { def is =
-  "GET / on TimelineServlet"                     ^
-    "should return status 302"                  ! root302^
-                                                end
+class TimelineServletSpec extends MutableScalatraSpec {
 
-  addServlet(classOf[TokensController], "/*")
-
-  def root302 = get("/") {
-    status must_== 302
-  }
+  // TODO
 }
